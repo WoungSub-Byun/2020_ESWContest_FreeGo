0f1bc1874db1e332732908142eb089ab7dba998d
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-08-23 15:55:16 +0900

    update error from controller

--- orm/controller.py
+++ orm/controller.py
@@ -42,7 +42,7 @@ def login_user(id, pwd):
         if len(entry) == 0:
             return "id not found"
         else:
-            queries = db_session.query(USERS_TB).filter(USERS_TB.id == id).filter(USERS_TB.id == pwd)
+            queries = db_session.query(USERS_TB).filter(USERS_TB.id == id).filter(USERS_TB.pwd == pwd)
             entry = entry = [dict(id = q.id, pwd=q.pwd) for q in queries]
             if len(entry) == 0:
                 return "pwd is wrong"
